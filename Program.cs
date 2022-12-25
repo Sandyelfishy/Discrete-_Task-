768ac316dbdf1bff80c0afded6ea12ea29c4e4bf
Sandyelfishy <115483715+Sandyelfishy@users.noreply.github.com>
2022-12-26 01:44:40 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace prime_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startnum, endnum, i, j; 
+            Console.WriteLine("please, enter the starting number ");
+             startnum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" please, enter the end number ");
+             endnum = int.Parse (Console.ReadLine());
+
+            Console.WriteLine("the list of the prime numbers from stating number ana end number is :");
+
+            for (i = startnum;i<= endnum; i++)
+            {     if (i==0 || i==1)
+                    continue;
+                for ( j = 2; j <=i; j++)
+                {
+                    if (i % j == 0)
+                        break;                   
+                }
+              if (i==j)
+                    Console.WriteLine(i);
+            } 
+        }
+    }
+}
